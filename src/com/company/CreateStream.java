9bc05df0568857fb799d24dc492391e3b7f70a04
nazarchykur <nazarchykur@gmail.com>
2019-09-23 15:39:22 +0300

    uncomment all code

--- src/com/company/CreateStream.java
+++ src/com/company/CreateStream.java
@@ -17,66 +17,66 @@ import java.util.stream.Stream;
 public class CreateStream {
     // Способи створення стрімів
     private static void testBuildStream() throws Exception {
-//        System.out.println("Test buildStream start");
-//
-//        // створення стріму із значень
-//        Stream<String> streamFromValues = Stream.of("a1", "a2", "a3");
-//        System.out.println("streamFromValues = " + streamFromValues.collect(Collectors.toList())); // виведе streamFromValues = [a1, a2, a3]
+        System.out.println("Test buildStream start");
 
-//        // створення стріму із массиву
-//        String[] array = {"a1","a2","a3"};
-//        Stream<String> streamFromArrays = Arrays.stream(array);
-//        System.out.println("streamFromArrays = " + streamFromArrays.collect(Collectors.toList())); // виведе streamFromArrays = [a1, a2, a3]
-//
-//        Stream<String> streamFromArrays_1 = Stream.of(array);
-//        System.out.println("streamFromArrays_1 = " + streamFromArrays_1.collect(Collectors.toList()));
+        // створення стріму із значень
+        Stream<String> streamFromValues = Stream.of("a1", "a2", "a3");
+        System.out.println("streamFromValues = " + streamFromValues.collect(Collectors.toList())); // виведе streamFromValues = [a1, a2, a3]
+
+        // створення стріму із массиву
+        String[] array = {"a1","a2","a3"};
+        Stream<String> streamFromArrays = Arrays.stream(array);
+        System.out.println("streamFromArrays = " + streamFromArrays.collect(Collectors.toList())); // виведе streamFromArrays = [a1, a2, a3]
+
+        Stream<String> streamFromArrays_1 = Stream.of(array);
+        System.out.println("streamFromArrays_1 = " + streamFromArrays_1.collect(Collectors.toList()));
 
 
-//        // створення стріму із файлу (кожен запис у файлі буде окремим рядком у стрімі)
-//        File file = new File("file_1.tmp");
-//        file.deleteOnExit();
-//        PrintWriter out = new PrintWriter(file);
-//        out.println("a1");
-//        out.println("a2");
-//        out.println("a3");
-//        out.close();
-//
-//        Stream<String> streamFromFiles = Files.lines(Paths.get(file.getAbsolutePath()));
-//        System.out.println("streamFromFiles = " + streamFromFiles.collect(Collectors.toList())); // виведе streamFromFiles = [a1, a2, a3]
-//
+        // створення стріму із файлу (кожен запис у файлі буде окремим рядком у стрімі)
+        File file = new File("file_1.tmp");
+        file.deleteOnExit();
+        PrintWriter out = new PrintWriter(file);
+        out.println("a1");
+        out.println("a2");
+        out.println("a3");
+        out.close();
 
-//        // створення стріму з колекції
+        Stream<String> streamFromFiles = Files.lines(Paths.get(file.getAbsolutePath()));
+        System.out.println("streamFromFiles = " + streamFromFiles.collect(Collectors.toList())); // виведе streamFromFiles = [a1, a2, a3]
+
+
+        // створення стріму з колекції
         Collection<String> collection = Arrays.asList("a1", "a2", "a3");
         Stream<String> streamFromCollection = collection.stream();
         System.out.println("streamFromCollection = " + streamFromCollection.collect(Collectors.toList())); // виведе streamFromCollection = [a1, a2, a3]
 
-//        // створення числового стріму з рядка
-//        IntStream streamFromString = "123".chars();
-//        System.out.print("streamFromString = ");
-//        streamFromString.forEach(e -> System.out.print(e + ", ")); // виведе streamFromString = 49 , 50 , 51 ,
-//        System.out.println();
+        // створення числового стріму з рядка
+        IntStream streamFromString = "123".chars();
+        System.out.print("streamFromString = ");
+        streamFromString.forEach(e -> System.out.print(e + ", ")); // виведе streamFromString = 49 , 50 , 51 ,
+        System.out.println();
 
-//        // з допомогою Stream.builder
-//        Stream.Builder<String> builder = Stream.builder();
-//        Stream<String> streamFromBuilder = builder.add("a1").add("a2").add("a3").build();
-//        System.out.println("streamFromBuilder = " + streamFromBuilder.collect(Collectors.toList())); // виведе streamFromFiles = [a1, a2, a3]
+        // з допомогою Stream.builder
+        Stream.Builder<String> builder = Stream.builder();
+        Stream<String> streamFromBuilder = builder.add("a1").add("a2").add("a3").build();
+        System.out.println("streamFromBuilder = " + streamFromBuilder.collect(Collectors.toList())); // виведе streamFromFiles = [a1, a2, a3]
 
-//        // створення безкінечних стрімів
-//        // з допомогою Stream.iterate
-//        Stream<Integer> streamFromIterate= Stream.iterate(1, n -> n + 2);
-//        System.out.println("streamFromIterate = " + streamFromIterate.limit(5).collect(Collectors.toList())); // виведе streamFromIterate = [1, 3, 5, 7, 9]
+        // створення безкінечних стрімів
+        // з допомогою Stream.iterate
+        Stream<Integer> streamFromIterate= Stream.iterate(1, n -> n + 2);
+        System.out.println("streamFromIterate = " + streamFromIterate.limit(5).collect(Collectors.toList())); // виведе streamFromIterate = [1, 3, 5, 7, 9]
 
-//        // з допомогою Stream.generate
-//        Stream<String> streamFromGenerate = Stream.generate(() -> "a1");
-//        System.out.println("streamFromGenerate = " + streamFromGenerate.limit(5).collect(Collectors.toList())); // виведе streamFromGenerate = [a1, a1, a1, a1, a1]
+        // з допомогою Stream.generate
+        Stream<String> streamFromGenerate = Stream.generate(() -> "a1");
+        System.out.println("streamFromGenerate = " + streamFromGenerate.limit(5).collect(Collectors.toList())); // виведе streamFromGenerate = [a1, a1, a1, a1, a1]
 
-//        // створення пустого стріму
-//        Stream<String> streamEmpty = Stream.empty();
-//        System.out.println("streamEmpty = " + streamEmpty.collect(Collectors.toList())); // виведе streamEmpty = []
-//
-//        // створить паралельний стрім з колекції
-//        Stream<String> parallelStream = collection.parallelStream();
-//        System.out.println("parallelStream = " + parallelStream.collect(Collectors.toList())); // виведе parallelStream = [a1, a2, a3]
+        // створення пустого стріму
+        Stream<String> streamEmpty = Stream.empty();
+        System.out.println("streamEmpty = " + streamEmpty.collect(Collectors.toList())); // виведе streamEmpty = []
+
+        // створить паралельний стрім з колекції
+        Stream<String> parallelStream = collection.parallelStream();
+        System.out.println("parallelStream = " + parallelStream.collect(Collectors.toList())); // виведе parallelStream = [a1, a2, a3]
 
 //  створить стрім з списку файлів по визначеному шляху
         Stream<Path> streamFromPath = Files.list(Paths.get(""));

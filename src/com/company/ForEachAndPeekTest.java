bbd0bf649c2568a43ec7536e4bda8a0dd1d09180
nazarchykur <nazarchykur@gmail.com>
2019-09-26 16:37:12 +0300

    Stream API forEach and peek methods

---
+++ src/com/company/ForEachAndPeekTest.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ForEachAndPeekTest {
+
+    //  ForEach і Peek по суті роблять одне і теж,
+    //  змінюють властивості об'єктів в Strim,
+    //  єдина різниця між ними в тому що ForEach термінальна і вона закінчує роботу зі стрімом,
+    //  в той час як Peek конвеєрна і робота зі стрімом триває.
+
+    // Метод ForEach застосовує вказаний метод до кожного елементу стріму і закінчує роботу зі стрімом
+
+    private static void methodForEach() {
+        System.out.println("For each start");
+        Collection<String> collection = Arrays.asList("a1", "a2", "a3", "a1");
+        // вивести інфо по кожному елементі стріму
+        System.out.print("forEach = ");
+        collection.stream()
+                .map(String::toUpperCase)
+                .forEach((e) -> System.out.print(e + ",")); // print forEach = A1,A2,A3,A1,
+
+        System.out.println();
+
+        Collection<StringBuilder> list = Arrays.asList(
+                new StringBuilder("a1"),
+                new StringBuilder("a2"),
+                new StringBuilder("a3")
+        );
+        list.stream()
+                .forEachOrdered((p) -> p.append("_new"));
+        System.out.println("forEachOrdered = " + list); // print forEachOrdered = [a1_new, a2_new, a3_new]
+    }
+
+    // Метод Peek повертає той же стрім, але при цьому застосовує вказаний метод до кожного елементу стріму
+    private static void methodPeek() {
+        System.out.println();
+        System.out.println("Test peek start");
+        Collection<String> collection = Arrays.asList("a1", "a2", "a3", "a1");
+
+        // напечатає дебажне інфо по кожному елементі стріма
+        System.out.print("peak1 = ");
+        List<String> peek = collection.stream()
+                .map(String::toUpperCase)
+                .peek(e -> System.out.print(e + ","))
+                .collect(Collectors.toList());
+        System.out.println();                 // print peak1 = A1,A2,A3,A1,
+        System.out.println("peek = " + peek); // print peek = [A1, A2, A3, A1]
+
+        Collection<StringBuilder> list = Arrays.asList(
+                new StringBuilder("a1"),
+                new StringBuilder("a2"),
+                new StringBuilder("a3")
+        );
+        List<StringBuilder> newList = list.stream()
+                .peek( p -> p.append("_new") )
+                .collect(Collectors.toList());
+        System.out.println("newList = " + newList); // print newList = [a1_new, a2_new, a3_new]
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        methodForEach();
+        methodPeek();
+    }
+}

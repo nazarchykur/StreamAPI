ae9bc9984afe03a101079dec288cba8d02ab40fd
nazarchykur <nazarchykur@gmail.com>
2019-09-26 15:20:27 +0300

    Stream API sorted method

---
+++ src/com/company/SortedTest.java
@@ -0,0 +1,134 @@
+package com.company;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class SortedTest {
+
+    // Метод Sorted дозволяє сортувати значення
+    // або в натуральному порядку, або задаючи Comparator
+    public void testSorted() {
+        System.out.println("Test sorted start");
+
+        // ************ Робота з рядками
+        Collection<String> collection = Arrays.asList("a1", "a4", "a3", "a2", "a1", "a4");
+
+        // впорядкувати значення за алфавітом
+        List<String> sorted = collection.stream()
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("sorted = " + sorted); // print sorted = [a1, a1, a2, a3, a4, a4]
+
+
+        // впорядкувати значення за алфавітом і прибрати дублікати
+        List<String> sortedDistinct = collection.stream()
+                .sorted()
+                .distinct()                   // don't print duplicate
+                .collect(Collectors.toList());
+        System.out.println("sortedDistinct = " + sortedDistinct); // print sortedDistinct = [a1, a2, a3, a4]
+
+        // впорядкувати значення за алфавітом у зворотному порядку
+        List<String> sortedReverse = collection.stream()
+                .sorted((o1, o2) -> -o1.compareTo(o2))
+                .collect(Collectors.toList());
+        System.out.println("sortedReverse = " + sortedReverse); // print sortedReverse = [a4, a4, a3, a2, a1, a1]
+        List<String> sortedReverse_1 = collection.stream()
+                .sorted(Comparator.reverseOrder())     //  Comparator.reverseOrder() = (o1, o2) -> -o1.compareTo(o2)
+                .collect(Collectors.toList());
+        System.out.println("sortedReverse_1 = " + sortedReverse_1); // print the sane result
+
+
+        // ************ Робота з об'єктами
+        // створимо колекцію людей
+        Collection<People> people = Arrays.asList(
+                new People("Leo", 20, Sex.MAN),
+                new People("Doni", 25, Sex.MAN),
+                new People("MJey", 18, Sex.WOMAN),
+                new People("OldMan", 70, Sex.MAN)
+        );
+
+        // Фільтр по імені в зворотному алфавітному порядку
+        Collection<People> byName = people.stream()
+                //.sorted((o1, o2) -> o1.getName().compareTo(o2.getName())) // with lambda
+                .sorted(Comparator.comparing(People::getName)) // with ref func
+                .collect(Collectors.toList());
+        System.out.println("byName = " + byName); // byName = [People{name='Doni', age=25, sex=MAN}, People{name='Leo', age=20, sex=MAN}, People{name='MJey', age=18, sex=WOMAN}, People{name='OldMan', age=70, sex=MAN}]
+
+        // Відсортувати спочатку за статтю, а потім за віком
+        Collection<People> bySexAndAge = people.stream()
+                // using lambda
+                //.sorted( (o1, o2)-> o1.getSex() != o2.getSex() ? o1.getSex().compareTo(o2.getSex()) : o1.getAge().compareTo(o2.getAge()))
+
+                // using ref func
+                // спочатку порівнюємо по статті, а потім за віком
+                // використовуємо thenComparing щоб зберегти порядок і логіку сортування
+                .sorted(Comparator.comparing(People::getSex).thenComparing(People::getAge))
+                .collect(Collectors.toList());
+        System.out.println("bySexAndAge = " + bySexAndAge); // print bySexAndAge = [People{name='Leo', age=20, sex=MAN}, People{name='Doni', age=25, sex=MAN}, People{name='OldMan', age=70, sex=MAN}, People{name='MJey', age=18, sex=WOMAN}]
+
+    }
+
+
+
+    private enum Sex {
+        MAN,
+        WOMAN
+    }
+
+    private class People {
+
+        private final String name;
+        private final Integer age;
+        private final Sex sex;
+
+        public People(String name, Integer age, Sex sex) {
+            this.name = name;
+            this.age = age;
+            this.sex = sex;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public Sex getSex() {
+            return sex;
+        }
+
+        @Override
+        public String toString() {
+            return "People{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    ", sex=" + sex +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            People people = (People) o;
+            return Objects.equals(name, people.name) &&
+                    Objects.equals(age, people.age) &&
+                    sex == people.sex;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, age, sex);
+        }
+    }
+
+    public static void main(String[] args) {
+        SortedTest sortedTest = new SortedTest();
+        sortedTest.testSorted();
+
+    }
+
+}
